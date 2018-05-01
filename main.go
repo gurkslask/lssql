b9cb89a20d7f1505fd69ae045291163841da3676
Alexander Svensson <gurkslask@gmail.com>
2018-05-01 21:18:55 +0200

    Ready with printtable and printtables function

--- main.go
+++ main.go
@@ -9,20 +9,32 @@ import (
 )
 
 func main() {
-	fmt.Println("vim-go")
-	var path = flag.String("path", "./", "Path to .db file")
-	var table = flag.String("table", "default", "Name of the table in db")
+	var path = flag.String("path", "/home/alex/data.db", "Path to .db file")
+	var table = flag.String("table", "", "Name of the table in db")
 	flag.Parse()
 	db, err := connectDB(path)
+	defer db.Close()
 	if err != nil {
 		fmt.Println(err)
 	}
-	printTable(db, table)
+
+	if *table == "" {
+		tables, err := printTables(db)
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println("No table provided, listed tables: \n", tables)
+	} else {
+		result, err := printTable(db, table)
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(result)
+	}
 
 }
 
 func connectDB(path *string) (*sql.DB, error) {
-	fmt.Println("Connect to a database")
 	db, err := sql.Open("sqlite3", *path)
 	if err != nil {
 		return nil, err
@@ -31,6 +43,39 @@ func connectDB(path *string) (*sql.DB, error) {
 }
 
 func printTable(db *sql.DB, tablename *string) (string, error) {
+	var result string
 	fmt.Println("Table")
-	return "", nil
+	rows, err := db.Query("select * from PEOPLE")
+	if err != nil {
+		return "", err
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var id, age int
+		var name string
+		err = rows.Scan(&id, &name, &age)
+		if err != nil {
+			return "", err
+		}
+		result += fmt.Sprint("+", id, "+", name, "+", age, "+\n")
+	}
+	return result, nil
+}
+
+func printTables(db *sql.DB) (string, error) {
+	var result string
+	rows, err := db.Query(`SELECT name FROM sqlite_master WHERE type = "table"`)
+	if err != nil {
+		return "", err
+	}
+	for rows.Next() {
+
+		var name string
+		err = rows.Scan(&name)
+		if err != nil {
+			return "", err
+		}
+		result += fmt.Sprint(name, "\n")
+	}
+	return result, nil
 }
