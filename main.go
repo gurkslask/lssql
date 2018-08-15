ba16cbcd60cd0912b4bf2101697b1400c6a651a6
Alex <gurkslask@gmail.com>
2018-08-15 22:09:13 +0200

    Changed queries to prepared statements to avoid injections

--- main.go
+++ main.go
@@ -1,24 +1,21 @@
 package main
 
 import (
+	"database/sql"
 	"errors"
 	"flag"
 	"fmt"
-	"github.com/jmoiron/sqlx"
 	_ "github.com/mattn/go-sqlite3"
 	"log"
-	//"strings"
-	// "os"
-	//"strconv"
 )
 
 var debug bool = false
 var debugp *bool = &debug
 
 func main() {
-	var path = flag.String("path", "", "Path to .db file")
+
+	var path = flag.String("path", "", "Path to db")
 	var table = flag.String("table", "", "Name of the table in db")
-	var col_id = flag.String("col_id", "id", "Name of the id column")
 	var limit = flag.Int("limit", 10, "Number of lines to print")
 	var offset = flag.Int("offset", 1, "Offset from where to start printing")
 	var debugf = flag.Bool("debug", false, "Prints extra debug info")
@@ -27,6 +24,7 @@ func main() {
 
 	if *debugp {
 		fmt.Println("In main")
+		fmt.Println(table)
 	}
 
 	db, err := connectDB(path)
@@ -48,7 +46,7 @@ func main() {
 		}
 		fmt.Println("No table provided, listed tables: \n", tables)
 	} else {
-		result, err := printTable(db, table, col_id, limit, offset)
+		result, err := printTable(db, table, limit, offset)
 		if err != nil {
 			fmt.Println(err)
 		}
@@ -58,11 +56,11 @@ func main() {
 }
 
 //Connect to database and return a db
-func connectDB(path *string) (*sqlx.DB, error) {
+func connectDB(path *string) (*sql.DB, error) {
 	if *debugp {
 		fmt.Println("In connect")
 	}
-	db, err := sqlx.Open("sqlite3", *path)
+	db, err := sql.Open("sqlite3", *path)
 	if err != nil {
 		return nil, err
 	}
@@ -70,14 +68,14 @@ func connectDB(path *string) (*sqlx.DB, error) {
 }
 
 //Gets strings from unknown columns
-func getData(db *sqlx.DB, query string) ([][]string, error) {
-	if *debugp {
+func getData(rows *sql.Rows) ([][]string, error) {
+	/*if *debugp {
 		fmt.Printf("In getdata with query %s\n", query)
 	}
 	rows, err := db.Query(query)
 	if err != nil {
 		return nil, err
-	}
+	}*/
 	if *debugp {
 		fmt.Println("Query done")
 	}
@@ -91,6 +89,7 @@ func getData(db *sqlx.DB, query string) ([][]string, error) {
 	var rresult [][]string
 	rownumber := 0
 
+	/* dest is where Scan puts data, make dest contain pointers to rawResult that is []byte */
 	dest := make([]interface{}, len(columns))
 	for i, _ := range rawResult {
 		dest[i] = &rawResult[i]
@@ -105,6 +104,7 @@ func getData(db *sqlx.DB, query string) ([][]string, error) {
 		if err != nil {
 			return nil, err
 		}
+		/* If rawresult isnt nil, make them string */
 		for i, raw := range rawResult {
 			if raw == nil {
 				result[i] = "\\N"
@@ -112,31 +112,56 @@ func getData(db *sqlx.DB, query string) ([][]string, error) {
 				result[i] = string(raw)
 			}
 		}
-		//Initialize and copy
+		//Initialize and copy to a [][]string that contains all the rows
 		rresult = append(rresult, make([]string, len(result)))
 		copy(rresult[rownumber], result)
 		rownumber += 1
 	}
-	if *debugp {
+	/*if *debugp {
 		fmt.Printf("out getdata with query %s\n", query)
-	}
+	}*/
 	return rresult, nil
 }
 
-func printTable(db *sqlx.DB, tablename, id_col *string, limit, offset *int) (string, error) {
+func printTable(db *sql.DB, tablename *string, limit, offset *int) (string, error) {
 	// Get data with queries and print it nicely with padding
 	if *debugp {
 		fmt.Println("*************In printTable")
 		fmt.Println("In Printtable with tablename: %s", *tablename)
 
 	}
-	heads, err := getData(db, fmt.Sprintf("PRAGMA table_info(%v)", *tablename))
+	tx, err := db.Begin()
 	if err != nil {
 		return "", err
 	}
-
 	fmt.Printf("Table %s\n\n", *tablename)
-	data, err := getData(db, fmt.Sprintf("SELECT * FROM %s LIMIT %d OFFSET %d ", *tablename, *limit, *offset))
+	//fmt.Print("PRAGMA TABLE_INFO(%s)", *tablename)
+	stmt, err := tx.Prepare(fmt.Sprintf("PRAGMA TABLE_INFO(%s)", *tablename))
+	if err != nil {
+		fmt.Println("stmt")
+		return "", err
+	}
+	rows, err := stmt.Query()
+	if err != nil {
+		fmt.Println("rows")
+		return "", err
+	}
+	heads, err := getData(rows)
+	if err != nil {
+		return "", err
+	}
+	fmt.Printf("Table %s\n\n", *tablename)
+	//data, err := getData(db, fmt.Sprintf("SELECT * FROM %s LIMIT %d OFFSET %d ", *tablename, *limit, *offset))
+	//stmt, err = db.Prepare("SELECT * FROM ? LIMIT (?) OFFSET (?) ")
+	stmt, err = db.Prepare(fmt.Sprintf("SELECT * FROM %s LIMIT ? OFFSET ? ", *tablename))
+	if err != nil {
+		return "", err
+	}
+	rows, err = stmt.Query(*limit, *offset)
+	if err != nil {
+		return "", err
+	}
+	data, err := getData(rows)
 	if err != nil {
 		return "", err
 	}
@@ -186,7 +211,7 @@ func padString(data string, length int, dest *string) {
 }
 
 //If no tables are found or requested, print available tables
-func printTables(db *sqlx.DB) (string, error) {
+func printTables(db *sql.DB) (string, error) {
 	if *debugp {
 		fmt.Println("In printTables")
 	}
